c2ad711928af13772bec2726234b536552677bb4
ladislas <ladislas@weareleka.com>
2014-08-11 16:56:14 +0200

    minor look changes

--- avr-libc.rb
+++ avr-libc.rb
@@ -1,27 +1,32 @@
 require 'formula'
 
 class AvrLibc < Formula
-  url 'http://download.savannah.gnu.org/releases/avr-libc/avr-libc-1.8.0.tar.bz2'
-  homepage 'http://www.nongnu.org/avr-libc/'
-  sha1 '2e3815221be8e22f5f2c07b922ce92ecfa85bade'
+	url 'http://download.savannah.gnu.org/releases/avr-libc/avr-libc-1.8.0.tar.bz2'
+	homepage 'http://www.nongnu.org/avr-libc/'
+	sha1 '2e3815221be8e22f5f2c07b922ce92ecfa85bade'
 
-  depends_on 'avr-gcc'
+	depends_on 'avr-gcc'
 
-  def install
-    ENV.delete 'CFLAGS'
-    ENV.delete 'CXXFLAGS'
-    ENV.delete 'LD'
-    ENV.delete 'CC'
-    ENV.delete 'CXX'
+	def install
+		ENV.delete 'CFLAGS'
+		ENV.delete 'CXXFLAGS'
+		ENV.delete 'LD'
+		ENV.delete 'CC'
+		ENV.delete 'CXX'
 
-    avr_gcc = Formula['avr-gcc']
-    build = `./config.guess`.chomp
-    system "./configure", "--build=#{build}", "--prefix=#{prefix}", "--host=avr"
-    system "make install"
-    avr = File.join prefix, 'avr'
-    # copy include and lib files where avr-gcc searches for them
-    # this wouldn't be necessary with a standard prefix
-    ohai "copying #{avr} -> #{avr_gcc.prefix}"
-    cp_r avr, avr_gcc.prefix
-  end
+		avr_gcc = Formula['avr-gcc']
+
+		build = `./config.guess`.chomp
+
+		system "./configure", "--build=#{build}", "--prefix=#{prefix}", "--host=avr"
+		system "make install"
+
+		avr = File.join prefix, 'avr'
+
+		# copy include and lib files where avr-gcc searches for them
+		# this wouldn't be necessary with a standard prefix
+		ohai "copying #{avr} -> #{avr_gcc.prefix}"
+		cp_r avr, avr_gcc.prefix
+
+	end
 end
