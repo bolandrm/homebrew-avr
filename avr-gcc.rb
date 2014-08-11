c1d5dda2419daa87c1623c04d8b8381bd4776cb0
ladislas <ladislas@weareleka.com>
2014-08-11 16:54:33 +0200

    minor look changes

--- avr-gcc.rb
+++ avr-gcc.rb
@@ -4,62 +4,62 @@ require 'formula'
 # `avr-gcc -print-prog-name=cc1plus` -v
 
 class AvrGcc < Formula
-  homepage 'http://www.gnu.org/software/gcc/gcc.html'
-  url 'http://ftpmirror.gnu.org/gcc/gcc-4.9.1/gcc-4.9.1.tar.bz2'
-  mirror 'ftp://gcc.gnu.org/pub/gcc/releases/gcc-4.9.1/gcc-4.9.1.tar.bz2'
-  sha1 '3f303f403053f0ce79530dae832811ecef91197e'
+	homepage 'http://www.gnu.org/software/gcc/gcc.html'
+	url 'http://ftpmirror.gnu.org/gcc/gcc-4.9.1/gcc-4.9.1.tar.bz2'
+	mirror 'ftp://gcc.gnu.org/pub/gcc/releases/gcc-4.9.1/gcc-4.9.1.tar.bz2'
+	sha1 '3f303f403053f0ce79530dae832811ecef91197e'
 
-  depends_on 'gmp'
-  depends_on 'libmpc'
-  depends_on 'mpfr'
-  depends_on 'cloog'
-  depends_on 'isl'
+	depends_on 'gmp'
+	depends_on 'libmpc'
+	depends_on 'mpfr'
+	depends_on 'cloog'
+	depends_on 'isl'
 
-  depends_on 'avr-binutils'
+	depends_on 'avr-binutils'
 
-  option 'disable-cxx', 'Don\'t build the g++ compiler'
+	option 'disable-cxx', 'Don\'t build the g++ compiler'
 
-  def install
-    # The C compiler is always built, C++ can be disabled
-    languages = %w[c]
-    languages << 'c++' unless build.include? 'disable-cxx'
+	def install
+		# The C compiler is always built, C++ can be disabled
+		languages = %w[c]
+		languages << 'c++' unless build.include? 'disable-cxx'
 
-    args = [
-            "--target=avr",
-            "--prefix=#{prefix}",
+		args = [
+			"--target=avr",
+			"--prefix=#{prefix}",
 
-            "--enable-languages=#{languages.join(',')}",
-            "--with-gnu-as",
-            "--with-gnu-ld",
-            "--with-ld=#{Formula["avr-binutils"].opt_bin/'avr-ld'}",
-            "--with-as=#{Formula["avr-binutils"].opt_bin/'avr-as'}",
+			"--enable-languages=#{languages.join(',')}",
+			"--with-gnu-as",
+			"--with-gnu-ld",
+			"--with-ld=#{Formula["avr-binutils"].opt_bin/'avr-ld'}",
+			"--with-as=#{Formula["avr-binutils"].opt_bin/'avr-as'}",
 
-            "--disable-nls",
-            "--disable-shared",
-            "--disable-threads",
-            "--disable-libssp",
-            "--disable-libstdcxx-pch",
-            "--disable-libgomp",
+			"--disable-nls",
+			"--disable-shared",
+			"--disable-threads",
+			"--disable-libssp",
+			"--disable-libstdcxx-pch",
+			"--disable-libgomp",
 
-            "--with-gmp=#{Formula["gmp"].opt_prefix}",
-            "--with-mpfr=#{Formula["mpfr"].opt_prefix}",
-            "--with-mpc=#{Formula["libmpc"].opt_prefix}",
-            "--with-cloog=#{Formula["cloog"].opt_prefix}",
-            "--with-isl=#{Formula["isl"].opt_prefix}",
-            "--with-system-zlib"
-    ]
+			"--with-gmp=#{Formula["gmp"].opt_prefix}",
+			"--with-mpfr=#{Formula["mpfr"].opt_prefix}",
+			"--with-mpc=#{Formula["libmpc"].opt_prefix}",
+			"--with-cloog=#{Formula["cloog"].opt_prefix}",
+			"--with-isl=#{Formula["isl"].opt_prefix}",
+			"--with-system-zlib"
+		]
 
-    mkdir 'build' do
-      system "../configure", *args
-      system "make"
+		mkdir 'build' do
+			system "../configure", *args
+			system "make"
 
-      ENV.deparallelize
-      system "make install"
-    end
+			ENV.deparallelize
+			system "make install"
+		end
 
-    # info and man7 files conflict with native gcc
-    info.rmtree
-    man7.rmtree
-  end
+		# info and man7 files conflict with native gcc
+		info.rmtree
+		man7.rmtree
+	end
 end
 
