6cdd5f0b9495008ff163a18bc8009aec6665d92f
Ladislas <ladislas@weareleka.com>
2014-11-11 17:00:29 +0100

    try accessing variable from another formulae

--- avr-gcc.rb
+++ avr-gcc.rb
@@ -10,7 +10,7 @@ class AvrGcc < Formula
 	mirror 'ftp://gcc.gnu.org/pub/gcc/releases/gcc-4.9.2/gcc-4.9.2.tar.bz2'
 	sha256 '2020c98295856aa13fda0f2f3a4794490757fc24bcca918d52cc8b4917b972dd'
 
-	avr_gcc_version = "4.9.2"
+	attr_accessor :avr_gcc_version = "4.9.2"
 
 	depends_on 'gmp'
 	depends_on 'libmpc'
