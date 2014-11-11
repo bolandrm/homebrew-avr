9ced2b9ee9d4680c9ef1391067b715d45c505d3f
Ladislas <ladislas@weareleka.com>
2014-11-11 13:24:40 +0100

    add avr-gcc 4.8 formulas

---
+++ avr-binutils48.rb
@@ -0,0 +1,39 @@
+require 'formula'
+
+class AvrBinutils48 < Formula
+	homepage 'http://www.gnu.org/software/binutils/binutils.html'
+	url 'http://ftpmirror.gnu.org/binutils/binutils-2.24.tar.gz'
+	mirror 'http://ftp.gnu.org/gnu/binutils/binutils-2.24.tar.gz'
+	sha1 '1b2bc33003f4997d38fadaa276c1f0321329ec56'
+
+	keg_only 'This formula was not linked to /usr/loca/bin to avoid conflicts with the default avr-binutils used with avr-gcc 4.9. To use avr-gcc 4.8, unlink all the binaries related to avr-gcc 4.9 before linking this one.'
+
+	# Support for -C in avr-size. See issue
+	# https://github.com/larsimmisch/homebrew-avr/issues/9
+	patch :p0 do
+		url 'https://gist.github.com/larsimmisch/4190960/raw/b36f3d6d086980006f097ae0acc80b3ada7bb7b1/avr-binutils-size.patch'
+		sha1 'b6d1ff7084b1f0a3fd2dee5383019ffb202e6c9a'
+	end
+
+	def install
+		args = [
+			"--disable-debug",
+			"--disable-dependency-tracking",
+			"--target=avr",
+			"--prefix=#{prefix}",
+			"--infodir=#{info}",
+			"--mandir=#{man}",
+			"--disable-werror",
+			"--disable-nls"
+		]
+
+		mkdir 'build' do
+			system "../configure", *args
+
+			system "make"
+			system "make install"
+		end
+
+		info.rmtree # info files conflict with native binutils
+	end
+end
---
+++ avr-libc48.rb
@@ -0,0 +1,34 @@
+require 'formula'
+
+class AvrLibc48 < Formula
+	url 'http://download.savannah.gnu.org/releases/avr-libc/avr-libc-1.8.1.tar.bz2'
+	homepage 'http://www.nongnu.org/avr-libc/'
+	sha256 'c3062a481b6b2c6959dc708571c00b0e26301897ba21171ed92acd0af7c4a969'
+
+	keg_only 'This formula was not linked to /usr/loca/bin to avoid conflicts with the default avr-libc used with avr-gcc 4.9. To use avr-gcc 4.8, unlink all the binaries related to avr-gcc 4.9 before linking this one.'
+
+	depends_on 'avr-gcc48'
+
+	def install
+		ENV.delete 'CFLAGS'
+		ENV.delete 'CXXFLAGS'
+		ENV.delete 'LD'
+		ENV.delete 'CC'
+		ENV.delete 'CXX'
+
+		avr_gcc = Formula['avr-gcc48']
+
+		build = `./config.guess`.chomp
+
+		system "./configure", "--build=#{build}", "--prefix=#{prefix}", "--host=avr"
+		system "make install"
+
+		avr = File.join prefix, 'avr'
+
+		# copy include and lib files where avr-gcc searches for them
+		# this wouldn't be necessary with a standard prefix
+		ohai "copying #{avr} -> #{avr_gcc.prefix}"
+		cp_r avr, avr_gcc.prefix
+
+	end
+end
